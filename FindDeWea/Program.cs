5b26067d66db27c716fffb67683084584cf2f351
Z <aerodinamicbg@gmail.com>
2018-09-27 11:41:21 +0300

    FindTheWay through a matrix App

---
+++ FindDeWea/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDeWea
+{
+    public class Program
+    {
+        private const string NO_PASS_SYMBOL = "X";
+        private const string PASS = ".";
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string[,] grid = new string[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                var gridItem = Console.ReadLine().Split(' ').ToArray();
+                for (int element = 0; element < gridItem.Length; element++)
+                {
+                    grid[i, element] = gridItem[element];
+                }
+            }
+
+            string[] startXStartY = Console.ReadLine().Split(' ');
+
+            int startX = Convert.ToInt32(startXStartY[0]);
+
+            int startY = Convert.ToInt32(startXStartY[1]);
+
+            int goalX = Convert.ToInt32(startXStartY[2]);
+
+            int goalY = Convert.ToInt32(startXStartY[3]);
+
+            int result = minimumMoves(grid, startX, startY, goalX, goalY);
+
+            Console.WriteLine(result);
+        }
+
+        public static int minimumMoves(string[,] grid, int startX, int startY, int goalX, int goalY)
+        {
+            int counter = 0;
+
+            if (startX == goalX && startY == goalY)
+            {
+                return 1;
+            }
+
+            int up = MoveUp(grid, startX, startY, goalX, goalY);
+            int left = MoveLeft(grid, startX, startY, goalX, goalY);
+            int down = MoveDown(grid, startX, startY, goalX, goalY);
+            int right = MoveRight(grid, startX, startY, goalX, goalY);
+
+            counter += Math.Min(Math.Min(up, left), Math.Min(down, right));
+
+            return counter;
+        }
+
+        private static int MoveRight(string[,] grid, int startX, int startY, int goalX, int goalY)
+        {
+            while (startX + 1 < grid.GetLength(0) && grid[startX + 1, startY] == PASS)
+            {
+                startX += 1;
+            }
+
+            return minimumMoves(grid, startX, startY, goalX, goalY);
+        }
+
+        private static int MoveDown(string[,] grid, int startX, int startY, int goalX, int goalY)
+        {
+            while (startY + 1 < grid.GetLength(1) && grid[startX, startY + 1] != NO_PASS_SYMBOL)
+            {
+                startY += 1;
+            }
+
+            return minimumMoves(grid, startX, startY, goalX, goalY);
+        }
+
+        private static int MoveLeft(string[,] grid, int startX, int startY, int goalX, int goalY)
+        {
+            while (startX - 1 > 0 && grid[startX - 1, startY] != NO_PASS_SYMBOL)
+            {
+                startX -= 1;
+            }
+
+            return minimumMoves(grid, startX, startY, goalX, goalY);
+        }
+
+        private static int MoveUp(string[,] grid, int startX, int startY, int goalX, int goalY)
+        {
+            while (startY - 1 > 0 && grid[startX, startY] != NO_PASS_SYMBOL)
+            {
+                startY -= 1;
+            }
+
+            return minimumMoves(grid, startX, startY, goalX, goalY);
+        }
+    }
+}
---
+++ FindDeWea/Tests/MinimumMoves_Tests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDeWea.Tests
+{
+    [TestClass]
+    public class MinimumMoves_Tests
+    {
+        [TestMethod]
+        public void MinimumMoves_Successful()
+        {
+            string[,] array = new string[3, 3]
+            {
+                {".", ".", "," },
+                {".", "X", "," },
+                {".", ".", "," }
+            };
+
+            int startX = 0;
+            int startY = 0;
+            int goalX = 1;
+            int goalY = 2;
+
+            var expectedMoves = 2;
+            var actualMoves = Program.minimumMoves(array, startX, startY, goalX, goalY);
+
+            Assert.AreEqual(expectedMoves, actualMoves);
+        }
+    }
+}
