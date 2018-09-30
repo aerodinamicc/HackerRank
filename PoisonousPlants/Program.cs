30bc5ca72acfc52a972b17511f8f23d7a6e118cc
Z <aerodinamicbg@gmail.com>
2018-09-30 12:57:24 +0300

    Poisonous plants

---
+++ PoisonousPlants/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoisonousPlants
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var arraySize = int.Parse(Console.ReadLine());
+            var array = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToList();
+
+            var daysCounter = 0;
+            var isArraySorted = false;
+
+            while (!isArraySorted)
+            {
+                var isIterationSorted = true;
+
+                for (int i = array.Count - 1; i > 0; i--)
+                {
+                    if (array[i] > array[i - 1])
+                    {
+                        array.RemoveAt(i);
+                        isIterationSorted = false;
+                    }
+                }
+
+                if (isIterationSorted)
+                {
+                    isArraySorted = true;
+                }
+                else
+                {
+                    daysCounter++;
+                }
+
+            }
+
+            Console.WriteLine(daysCounter);
+        }
+    }
+}
